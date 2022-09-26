74e83206058809c1c0c64c5a26f5aa85673b948f
CristianoAliotta <cristiano.aliotta@outlook.com>
2022-09-26 16:49:14 +0200

    interval function aded

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let dateTime = new Date();
-console.log(dateTime);
-let date = dateTime.toLocaleDateString("it-IT", { weekday: "long", day: "numeric", month: "long", year: "numeric" });
-console.log(date);
-document.getElementById("date").innerHTML = date;
-document.getElementById("hours").innerHTML = dateTime.getHours();
-document.getElementById("minutes").innerHTML = dateTime.getMinutes();
-document.getElementById("seconds").innerHTML = dateTime.getSeconds();
+setInterval(() => {
+  let dateTime = new Date();
+  let date = dateTime.toLocaleDateString("it-IT", { weekday: "long", day: "numeric", month: "long", year: "numeric" });
+  document.getElementById("date").innerHTML = date;
+  document.getElementById("hours").innerHTML = dateTime.getHours();
+  document.getElementById("minutes").innerHTML = dateTime.getMinutes();
+  document.getElementById("seconds").innerHTML = dateTime.getSeconds();
+}, 1000);
